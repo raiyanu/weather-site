a5e15fb51fbc6b2ab724d64902eadcadf10b8651
Raiyan Ahmed <ray38250968@gmail.com>
2024-12-27 16:35:58 +0530

    on progressive commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import Weather from "./weather.js";
 const weather = new Weather();
 
-weather.test();
+// weather.test();
 
 let TempWeather = {
     "location": {
@@ -50,3 +50,98 @@ let TempWeather = {
         "gust_kph": 19.1
     }
 }
+
+async function getUpdatedWeatherInfo(location) {
+    let weatherInfo =
+        await weather.fetchWeather(location);
+    updateUi(weatherInfo.current)
+}
+
+async function updateUiBasedByUserLocation() {
+    let weatherInfo = await weather.fetchWeather(await weather.getUserCordinate());
+    updateUi(weatherInfo);
+    alert('success')
+}
+
+async function updateUi(weatherInfo) {
+    try {
+        document.getElementById("ui-celsius").innerHTML = `${Math.floor(weatherInfo.current.temp_c)}°`;
+        document.getElementById("ui-word-condition").innerHTML = weatherInfo.current.condition.text;
+        document.getElementById("ui-date").innerHTML = weather.formatDate(weatherInfo.current.last_updated);
+        document.getElementById("weather-icon-condition").src = `//cdn.weatherapi.com/weather/64x64/day/${weatherInfo.current.condition.icon.split('/').pop()}`;
+        document.getElementById("ui-humidity").innerHTML = `${weatherInfo.current.humidity}%`;
+        document.getElementById("ui-cloud-cover").innerHTML = `${weatherInfo.current.cloud}%`;
+        document.getElementById("ui-wind-speed").innerHTML = `${Math.floor(weatherInfo.current.wind_mph)}mph ~ ${Math.floor(weatherInfo.current.wind_kph)}kph`;
+        document.getElementById("ui-feelslike").innerHTML = `${Math.floor(weatherInfo.current.feelslike_c)}°C`;
+        document.getElementById("ui-pressure").innerHTML = `${weatherInfo.current.pressure_mb} mb`;
+        document.getElementById("ui-wind-gust").innerHTML = `${Math.floor(weatherInfo.current.gust_mph)}mph`;
+        document.getElementById("ui-visibility").innerHTML = `${weatherInfo.current.vis_km}km`;
+        document.getElementById("ui-uv-index").innerHTML = `${weatherInfo.current.uv} Low`;
+        if (weatherInfo.current.alerts && Array.isArray(weatherInfo.current.alerts) && weatherInfo.current.alerts.length > 0) {
+            document.getElementById("ui-weather-alert").style.display = "block";
+            document.getElementById("alert-text").innerHTML = weatherInfo.current.alerts[0].description;
+        } else {
+            document.getElementById("ui-weather-alert").style.display = "none";
+        }
+    } catch (error) {
+        console.log(error);
+        alert(error.message);
+    }
+
+
+}
+
+
+window.addEventListener('DOMContentLoaded', async () => {
+    // getUpdatedWeatherInfo(await weather.getUserCordinate());
+    let TempWeather = {
+        current: {
+            "last_updated_epoch": 1735292700,
+            "last_updated": "2024-12-27 15:15",
+            "temp_c": 24.8,
+            "temp_f": 76.7,
+            "is_day": 1,
+            "condition": {
+                "text": "Light rain shower",
+                "icon": "//cdn.weatherapi.com/weather/64x64/day/353.png",
+                "code": 1240
+            },
+            "wind_mph": 11.4,
+            "wind_kph": 18.4,
+            "wind_degree": 75,
+            "wind_dir": "ENE",
+            "pressure_mb": 1014,
+            "pressure_in": 29.94,
+            "precip_mm": 0.5,
+            "precip_in": 0.02,
+            "humidity": 80,
+            "cloud": 72,
+            "feelslike_c": 96.9,
+            "feelslike_f": 80.4,
+            "windchill_c": 24.8,
+            "windchill_f": 76.7,
+            "heatindex_c": 26.9,
+            "heatindex_f": 80.4,
+            "dewpoint_c": 21.2,
+            "dewpoint_f": 70.2,
+            "vis_km": 10,
+            "vis_miles": 6,
+            "uv": 1.5,
+            "gust_mph": 16.4,
+            "gust_kph": 26.4
+        }
+    }
+    // updateUi(TempWeather);
+    await updateUiBasedByUserLocation()
+
+
+    document.getElementById('ui_location_input').addEventListener('input', async () => {
+        console.log('hey there');
+        let suggestedCity = await weather.getCitySuggestion(document.getElementById('ui_location_input').value);
+        let location_suggestions_el = document.getElementById('location_suggestions');
+        // location_suggestions_el.innerHTML = ''
+        for (const city of suggestedCity) {
+            location_suggestions_el.innerHTML += `<option>${city.name}, ${city.region}</option>`;
+        }
+    });
+})
--- js/weather.js
+++ js/weather.js
@@ -1,46 +1,74 @@
 export default class Weather {
     getApiKey() {
-        return '34768342301d4b8eb51120026242612';
+        return "34768342301d4b8eb51120026242612";
     }
     getApi(location) {
-        return `http://api.weatherapi.com/v1/current.json?key=${this.getApiKey()}&q=${location}`
+        return `http://api.weatherapi.com/v1/current.json?key=${this.getApiKey()}&q=${location}`;
+    }
+    getSuggestionApi(typedInput) {
+        return `http://api.weatherapi.com/v1/search.json?key=${this.getApiKey()}&q=${typedInput}`;
     }
     constructor() { }
 
+    async getCitySuggestion(typedInput) {
+        const res = await fetch(this.getSuggestionApi(typedInput));
+        const data = await res.json();
+        console.log('data:', data)
+        let suggestedLocation = [];
+        data.forEach(suggestedCity => {
+            suggestedLocation.push({ name: suggestedCity.name, region: suggestedCity.region })
+        });
+        console.log('suggested location:', suggestedLocation)
+        return suggestedLocation;
+    }
+
     async test() {
         console.log("test");
-        this.fetchWeather(await this.getUserCordinate());
+        console.log(this.fetchWeather(await this.getUserCordinate()));
     }
     async fetchWeather(location) {
         const res = await fetch(this.getApi(location));
         const data = await res.json();
-        console.log(data);
+        return data;
     }
     async getUserCordinate() {
         let userLocation;
-        if ('permissions' in navigator) {
+        if ("permissions" in navigator) {
             try {
-                const permissionStatus = await navigator.permissions.query({ name: 'geolocation' });
-                console.log('Geolocation permission state is: ', permissionStatus.state);
-                if (permissionStatus.state === 'granted') {
-                    console.log('Permission granted, you can access location');
+                const permissionStatus = await navigator.permissions.query({
+                    name: "geolocation",
+                });
+                console.log(
+                    "Geolocation permission state is: ",
+                    permissionStatus.state
+                );
+                if (permissionStatus.state === "granted") {
+                    console.log("Permission granted, you can access location");
                     const position = await new Promise((resolve, reject) => {
                         navigator.geolocation.getCurrentPosition(resolve, reject);
                     });
-                    console.log(`${position.coords.latitude} : ${position.coords.longitude}`);
+                    console.log(
+                        `${position.coords.latitude} : ${position.coords.longitude}`
+                    );
                     userLocation = `${position.coords.latitude},${position.coords.longitude}`;
                     console.log("location grabbed");
-                } else if (permissionStatus.state === 'denied') {
-                    console.error('Permission denied');
-                } else if (permissionStatus.state === 'prompt') {
-                    console.log('Permission prompt is pending');
+                } else if (permissionStatus.state === "denied") {
+                    console.error("Permission denied");
+                } else if (permissionStatus.state === "prompt") {
+                    console.log("Permission prompt is pending");
                 }
             } catch (error) {
-                console.error('Permission query failed:', error);
+                console.error("Permission query failed:", error);
             }
-            console.log("User Location the one grabbed :", userLocation);
         } else {
         }
         return userLocation ? userLocation : `12.9333,78.7167`;
+    }
+    formatDate(inputDate) {
+        const date = new Date(inputDate);
+        const options = { weekday: 'long', day: 'numeric', month: 'long', year: '2-digit' };
+        const formattedDate = date.toLocaleDateString('en-US', options);
+        const [weekday, day, month, year] = formattedDate.split(' ');
+        return `${weekday} ${day}, ${month.charAt(0).toUpperCase() + month.slice(1)} '${year}`;
     }
 }
